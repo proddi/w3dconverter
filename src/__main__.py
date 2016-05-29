17c947075b0e3d4420ba8f935cde829be7d08c24
Proddi <proddi@splatterladder.com>
2016-05-29 22:03:55 +0200

    Added UV mapping.

--- src/__main__.py
+++ src/__main__.py
@@ -10,6 +10,7 @@ def main(args):
     parser = ArgumentParser()
     parser.add_argument("--debug", "-d", dest="debug", action="store_true", default=False, help="Verbose output")
     parser.add_argument("--file", required=True, type=str)
+    parser.add_argument("--texture-path", type=str, nargs="*")
     parser.add_argument("--threejs", type=str, help="Export as threejs object")
     options = parser.parse_args()
 
@@ -21,16 +22,18 @@ def main(args):
 
     if 0:
         import yaml
+        from mathutils import Vector, Quaternion
         yaml.add_representer(Vector, lambda dumper, vector: dumper.represent_scalar('Vector', '%s, %s, %s' % (vector.x, vector.y, vector.z)))
         yaml.add_representer(Quaternion, lambda dumper, q: dumper.represent_scalar('Quaternion', '%s, %s, %s, %s' % (q.w, q.x, q.y, q.z)))
         print(yaml.dump(model))
         exit(0)
 
     if options.threejs:
-        data = model.as_threejs()
+        data = model.as_threejs(texture_paths=options.texture_path)
         with open(options.threejs, "w") as fw:
             fw.write(json.dumps(data, indent=2))
-            print(json.dumps(data, indent=2))
+            if options.debug:
+                print(json.dumps(data, indent=2))
 
     else:
         print(json.dumps(model.as_threejs(), indent=2))
--- src/w3d/__init__.py
+++ src/w3d/__init__.py
@@ -4,6 +4,12 @@ from .reader import ReadLong, GetChunkSize, ReadMesh, ReadHierarchy, ReadAnimati
 import logging
 logger = logging.getLogger("W3DModel.w3d")
 
+from mathutils import Vector
+def t(vec):
+    return vec
+    return Vector((vec.z, vec.y, vec.x))
+
+
 
 class W3DModel(object):
 
@@ -68,7 +74,7 @@ class W3DModel(object):
         return Cls(Meshes, Hierarchy, Animation, CompressedAnimation, HLod, Box)
 
 
-    def as_threejs(self):
+    def as_threejs(self, texture_paths=[]):
         logger.debug("building threejs object...")
 
         if not self.hlod.header.modelName == self.hlod.header.HTreeName:
@@ -77,98 +83,67 @@ class W3DModel(object):
         mesh_index = { m.header.meshName: m for m in self.meshes }
 
         object_root = self.createHierarchyWrapper(self.hierarchy.pivots, self.meshes)
-        print("object_root", object_root)
-        """
-        pivot_names = { i: p.name for i, p in enumerate(self.hierarchy.pivots) }
-        print(pivot_names)
-        pivot_hierarchy = {}
-        for pivot in self.hierarchy.pivots:
-            if pivot.parentID != 4294967295:
-                pivot_hierarchy.setdefault(pivot_names[pivot.parentID], []).append(pivot)
 
-        print(pivot_hierarchy[pivot_names[0]])
-        print(pivot_hierarchy)
-
-
-#        raise NotImplementedError
-
-        for m in self.meshes:
-            logger.debug("preparing mesh %s" % m.header.meshName)
-            Faces = []
-            for f in m.faces:
-                Faces.append(Face(*f.vertIds, f.normal))
-
-#            print(Faces)
-
-            if len(m.matlPass.txStage.txCoords)>0:
-                index = 0
-                for f in m.faces:
-                    face = Faces[index]
-#                    print(
-#                        m.matlPass.txStage.txCoords[face.a],
-#                        m.matlPass.txStage.txCoords[face.b],
-#                        m.matlPass.txStage.txCoords[face.c]
-#                    )
-#                    f.loops[0][uv_layer].uv = m.matlPass.txStage.txCoords[Faces[index][0]]
-#                    f.loops[1][uv_layer].uv = m.matlPass.txStage.txCoords[Faces[index][1]]
-#                    f.loops[2][uv_layer].uv = m.matlPass.txStage.txCoords[Faces[index][2]]
-                    index+=1
-
-
-        """
+        materials = []
+        textures = []
 
         logger.info("Threejs object built")
 
         _object = object_root.to_json()
 
-        return {
-            "images": [],
+        for image in ImageWrapper.wrappings():
+            image.fix_file(texture_paths=texture_paths)
+
+        for image in ImageWrapper.wrappings():
+            image.export(ext=".jpg", folder="textures")
+
+        data = {
+            "images": [ image.to_json() for image in ImageWrapper.wrappings() if image.ready() ],
             "animations": [],
             "object": _object,
-            "textures": [],
+            "textures": [ texture.to_json() for texture in TextureWrapper.wrappings() if texture.ready() ],
             "metadata": {},
             "geometries": [ mesh.to_json() for mesh in MeshWrapper.wrappings() ],
-            "materials": []
+            "materials": [ material.to_json() for material in MaterialWrapper.wrappings() ]
         }
 
+        if 0:
+            data["materials"].append({
+                    "emissive": 0,
+                    "type": "MeshPhongMaterial",
+                    "name": "zzbuildingsizeMaterial",
+                    "ambient": 11579568,
+                    "blending": "NormalBlending",
+                    "uuid": "A4EB44DB-19EB-3E46-858B-2C653E96438F",
+                    "vertexColors": False,
+                    "depthWrite": True,
+                    "side": 2,
+                    "map": "1DED9F78-5CC5-39D4-A1BA-E382B36BBEF6",
+                    "shininess": 50,
+                    "specular": 0,
+                    "depthTest": True,
+                    "color": 11579568
+                })
 
-    def _as_threejs_object(self, pivot_hierarchy, mesh_index, pivot=None):
-        mesh = None
-        if pivot is None:  # search for root
-            pivots = pivot_hierarchy.get("ROOTTRANSFORM")
-            assert pivots, "No root pivot found"
-            assert len(pivots) == 1, "More than one root node found"
-            pivot = pivots[0]
+            data["textures"].append({
+                    "magFilter": 1006,
+                    "name": "zzbuildingsize_dds_jpg",
+                    "wrap": [1000,1000],
+                    "mapping": 300,
+                    "minFilter": 1008,
+                    "image": "0335B7DD-379B-3768-B834-4C9A343293D3",
+                    "anisotropy": 1,
+                    "uuid": "1DED9F78-5CC5-39D4-A1BA-E382B36BBEF6",
+                    "repeat": [1,1]
+                })
 
-            print(mesh_index)
-            return {
-                "type": "Scene",
-                "children": [ self._as_threejs_object(pivot_hierarchy, mesh_index, p) for p in pivot_hierarchy.get(pivot.name, []) ],
-                "matrix": [item for item in matrix4x4(pivot.position, pivot.rotation)],
-                "uuid": "A591ACA3-4DE6-4996-9D9A-%s" % pivot.name,
-            }
+            data["images"].append({
+                    "url": "zzbuildingsize.64x64.jpg",
+                    "uuid": "0335B7DD-379B-3768-B834-4C9A343293D3",
+                    "name": "zzbuildingsize.64x64.jpg"
+                })
 
-
-        if not mesh:
-            mesh = mesh_index.get(pivot.name)
-
-
-
-        return {
-            "type": "Mesh" if mesh is not None else "Unknown",
-            "name": pivot.name,
-            "uuid": "DDA9AE26-14F9-3E6A-9BDD-%s" % pivot.name,
-            "matrix": [item for item in matrix4x4(pivot.position, pivot.rotation)],
-            "visible": True,
-#            "material": "A4EB44DB-19EB-3E46-858B-2C653E96438F",
-            "castShadow": True,
-            "receiveShadow": True,
-            "geometry": mesh.header.meshName if mesh else None,
-            "children": [ self._as_threejs_object(pivot_hierarchy, mesh_index, p) for p in pivot_hierarchy.get(pivot.name, []) ],
-        }
-
-
-
+        return data
 
     def createHierarchyWrapper(self, pivots, meshes):
         # convert to wrappers
@@ -197,15 +172,16 @@ class W3DModel(object):
         scene.rotation = struct_w3d.Quaternion((0, 0, 0, 0))
         return scene
 
+
 class Wrapper(object):
     _cache = {}
 
     @classmethod
-    def wrap(Cls, wrapped):
+    def wrap(Cls, wrapped, *args, **kwargs):
         try:
             wrapper = Cls._cache[wrapped]
         except KeyError:
-            wrapper = Cls._cache[wrapped] = Cls(wrapped)
+            wrapper = Cls._cache[wrapped] = Cls(wrapped, *args, **kwargs)
         return wrapper
 
     def __init__(self, wrapped):
@@ -230,16 +206,29 @@ class Face(object):
         self.b = b
         self.c = c
         self.normal = normal
+        self.uvs = []
     def __repr__(self):
         return '<Face %s, %s, %s, normal=%s>' % (self.a, self.b, self.c, self.normal)
-    def to_tuple(self, normals):
-        return (
-            16,
-            self.a, self.b, self.c,
-            normals.index(self.normal)
-        )
+    def to_tuple(self, normals, uvs):
+        type = 0
+        normal = uv = material = ()
+        if self.normal:
+            type += 1 << 4
+            normal = (normals.index(self.normal),)
+        if self.uvs:
+            assert len(self.uvs) == 1
+            type += 1 << 3
+            uv = tuple( uvs.index(vec) for vec in self.uvs[0] )
+            type += 1 << 1
+            material = (0,)
+
+        return (type,) + (self.a, self.b, self.c) + material + uv + normal
 
 class MeshWrapper(Wrapper):
+    def __init__(self, *args, **kwargs):
+        super(MeshWrapper, self).__init__(*args, **kwargs)
+        self.materials = []
+
     def uuid(self):
         return "DDA9AE26-MESH-UUID-%s" % self.header.meshName
 
@@ -247,11 +236,28 @@ class MeshWrapper(Wrapper):
 
         faces = []
         normals = []
+        uvs = []
         for f in self.faces:
             faces.append(Face(*f.vertIds, f.normal))
             normals.append(f.normal)
 
-        return {
+        assert len(self.matlPass.vmIds) >= 1, self.header.meshName
+
+        if self.matlPass.vmIds != [0]:
+            logger.warn("%s: Multitexture isn't supported yet, just using first" % self.header.meshName)
+#        assert self.matlPass.vmIds == [0], self.header.meshName  # just one material is currently supported
+
+        if len(self.matlPass.txStage.txCoords)>0:
+            for face in faces:
+                uv = [
+                    self.matlPass.txStage.txCoords[face.a],  # Tuple
+                    self.matlPass.txStage.txCoords[face.b],
+                    self.matlPass.txStage.txCoords[face.c]
+                ]
+                face.uvs.append(uv)
+                uvs.extend(uv)
+
+        data = {
             "uuid": self.uuid(),
             "type": "Geometry",
             "data": {
@@ -266,11 +272,17 @@ class MeshWrapper(Wrapper):
 #                    "version": 3,
 #                    "materials": 1
                 },
-                "faces": [ item for face in faces for item in face.to_tuple(normals)],
-                "normals": [ item for n in normals for item in n.to_tuple() ],
-                "vertices": [ item for n in self.verts for item in n.to_tuple() ],
+                "faces": [ item for face in faces for item in face.to_tuple(normals, uvs)],
+                "normals": [ item for n in normals for item in t(n).to_tuple() ],
+                "vertices": [ item for n in self.verts for item in t(n).to_tuple() ],
             },
         }
+        if uvs:
+            data["data"]["metadata"]["uvs"] = 1
+            data["data"]["uvs"] = [ [ x for vec in uvs for x in vec ] ]
+
+        return data
+
 
 class PivotWrapper(Wrapper):
     def __init__(self, *args, **kwargs):
@@ -286,15 +298,250 @@ class PivotWrapper(Wrapper):
             "name": self.name,
             "type": "Mesh" if self.mesh is not None else "Other",
             "uuid": self.uuid(),
-            "matrix": [item for item in matrix4x4(self.position, self.rotation)],
+            "matrix": [item for item in matrix4x4(t(self.position), self.rotation)],
             "visible": True,
             "children": [ pivot.to_json() for pivot in self.children ],
         }
 
         if self.mesh:
             data["geometry"] = self.mesh.uuid()
+            data["material"] = createMaterial(self.mesh)
 
         return data
+
+def createMaterial(mesh):
+    assert len(mesh.vertMatls), mesh.header.meshName
+#    assert len(mesh.textures), mesh.header.meshName
+    assert len(mesh.vertMatls) >= 1, mesh.vertMatls
+#    assert len(mesh.textures) >= 1, mesh.textures
+    vertMatl = mesh.vertMatls[0]
+
+    names = [ matl.vmName for matl in mesh.vertMatls ] + [ tx.name for tx in mesh.textures ]
+
+    material = MaterialWrapper.wrap("-".join(names))
+
+    if mesh.textures:
+        texture = mesh.textures[0]
+        material.texture = TextureWrapper.wrap(texture.name)
+    material.vertMatl = vertMatl
+
+    return material.uuid()
+
+class MaterialWrapper(Wrapper):
+    _cache = {}
+    def __init__(self, *args, **kwargs):
+        super(MaterialWrapper, self).__init__(*args, **kwargs)
+        self.name = self._wrapped
+        self._wrapped = None
+        self.texture = None
+        self.vertMatl = None
+
+    def uuid(self):
+        return self.name + "-Material"
+
+    def to_json(self):
+        data = {
+            "uuid": self.uuid(),
+            "type": "MeshPhongMaterial",
+
+            "doubleSided": False,
+            "shading": "phong",
+            "blending": "NormalBlending",
+
+            "opacity": self.vertMatl.vmInfo.opacity,
+            "shininess": self.vertMatl.vmInfo.shininess,
+
+#            "diffuse": rgba_to_hex(self.vertMatl.vmInfo.diffuse),  # TODO: wrong numbers ??
+#            "ambient": rgba_to_hex(self.vertMatl.vmInfo.ambient),  # TODO: wrong numbers ??
+            "specular": rgba_to_hex(self.vertMatl.vmInfo.specular),  # TODO: wrong numbers ??
+            "emissive": rgba_to_hex(self.vertMatl.vmInfo.emissive),  # TODO: wrong numbers ??
+        }
+
+        if self.texture:
+            if self.texture.ready():
+                data["map"] = self.texture.ref()
+            else:
+                logger.warn("%s: texture isn't ready" % self.uuid())
+
+        return data
+
+class TextureWrapper(Wrapper):
+    _cache = {}
+    def __init__(self, *args, **kwargs):
+        super(TextureWrapper, self).__init__(*args, **kwargs)
+        self.name = self._wrapped
+        self._wrapped = None
+        self.image = ImageWrapper.wrap(self.name)
+
+    def ref(self):
+        return self.name + "-Texture"
+
+    def to_json(self):
+        data = {
+            "uuid": self.ref(),
+            "name": self.name,
+#            "anisotropy": 1,
+        }
+        if self.image.ready():
+            data["image"] = self.image.ref()
+        else:
+            logger.warn("%s: image isn't ready" % self.ref())
+        return data
+
+    def ready(self):
+        return self.image.ready()
+
+class ImageWrapper(Wrapper):
+    _cache = {}
+    def __init__(self, *args, **kwargs):
+        super(ImageWrapper, self).__init__(*args, **kwargs)
+        self.name = self._wrapped
+        self._wrapped = None
+        self.url = None
+        self.error = None
+
+    def ref(self):
+        return self.name + "-Image"
+
+    def to_json(self):
+        data = {
+            "uuid": self.ref(),
+            "name": self.name,
+        }
+        if self.ready():
+            data["url"] = self.url
+        return data
+
+    def ready(self):
+        return self.error is None
+
+    def fix_file(self, texture_paths=[]):
+        logger.debug(texture_paths)
+        import os.path
+        path, ext = os.path.splitext(self.name.lower())
+        for e in [ext] + [".dds", ".tga"]:
+            for root in texture_paths:
+                logger.debug("test: %s" % os.path.join(root, path+e))
+                if os.path.exists(os.path.join(root, path+e)):
+                    path = os.path.join(root, path)
+                    ext = e
+                    self.url = os.path.relpath("%s%s" % (path, ext), root)
+                    self.root = root
+                    logger.debug("found: %s" % self.url)
+                    return
+        raise FileNotFoundError(self.name)
+
+    def export(self, ext=".jpg", folder="."):
+        import os.path
+        path, e = os.path.splitext(self.url)
+
+        if e == ".jpg":
+            pass
+        elif e == ".dds":
+            to_url = os.path.join(folder, self.url+".jpg")
+
+            folder, a = os.path.split(to_url)
+            try:
+                os.makedirs(folder)
+            except FileExistsError:
+                pass
+
+            try:
+                logger.debug("convert: %s" % self.url)
+                from .dds import DDS
+                im = DDS(os.path.join(self.root, self.url))
+                im.save(to_url)
+                self.url = to_url
+            except ValueError as e:
+                logger.exception("Couldn't convert image - skipping...")
+                self.error = e
+        else:
+            raise NotSupportedError(e)
+
+
+class __MaterialWrapper(Wrapper):
+    def __init__(self, *args, **kwargs):
+        super(MaterialWrapper, self).__init__(*args, **kwargs)
+#        self.shaders = shaders
+#        self.children = []
+#        self.mesh = None
+
+    def uuid(self):
+        return self.vmName + "Material"
+
+    def to_json(self):
+        data = {
+#            "emissive": 0,
+            "type": "MeshPhongMaterial",
+#            "name": "zzbuildingsizeMaterial",
+#            "ambient": 11579568,
+#            "blending": "NormalBlending",
+            "uuid": self.uuid(),
+#            "vertexColors": False,
+#            "depthWrite": True,
+#            "side": 2,
+            "map": "1DED9F78-5CC5-39D4-A1BA-E382B36BBEF6",
+#            "shininess": 50,
+#            "specular": 0,
+#            "depthTest": True,
+#            "color": 11579568
+        }
+
+        return data
+
+    """
+    def to_mesh_json(self, mesh):
+        data = {
+                "DbgIndex": 0,
+                "DbgName": self.uuid(),
+                "DbgColor": 15658734,
+                "blending": "NormalBlending",
+                "visible": True,
+
+                "transparent": False,
+
+                "opacity": self.vmInfo.opacity,
+                "doubleSided": False,
+                "shading": "phong",
+                "mapDiffuseAnisotropy": 1,
+                "mapDiffuseRepeat": [1,1],
+#                    "mapDiffuse": "zzbuildingsize.64x64.jpg",
+#                    "mapDiffuseWrap": ["RepeatWrapping","RepeatWrapping"],
+                "depthTest": True,
+                "depthWrite": True,
+                "wireframe": False,
+                "specularCoef": 50,
+                "__colorDiffuse": [0.690196, 0.690196, 0.690196],
+                "colorDiffuse": rgba_to_rgbtuple(self.vmInfo.diffuse),  # TODO: wrong numbers ??
+
+                "__colorAmbient": [0.690196, 0.690196, 0.690196],
+                "colorAmbient": rgba_to_rgbtuple(self.vmInfo.ambient),  # TODO: wrong numbers ??
+
+                "__colorSpecular": [0.001176, 0.001176, 0.001176],
+                "colorSpecular": rgba_to_rgbtuple(self.vmInfo.specular),  # TODO: wrong numbers ??
+
+                "__colorEmissive": [0, 0, 0],
+                "colorEmissive": rgba_to_rgbtuple(self.vmInfo.emissive),  # TODO: wrong numbers ??
+                "foooo": "bar",
+            }
+
+        if len(mesh.shaders) > 0:
+            if mesh.shaders[0].alphaTest == 1:
+                raise NotImplementedError("shader.alphaTest")
+#                mat.use_transparency = True
+#                mat.transparency_method = "Z_TRANSPARENCY"
+            if mesh.shaders[0].destBlend == 1:
+                raise NotImplementedError("shader.destBlend")
+#                mat.use_transparency = True
+#                mat.transparency_method = "Z_TRANSPARENCY"
+#                destBlend = 1
+
+
+#        if not mesh.bumpMaps.normalMap.entryStruct.normalMap == "":
+#            raise NotImplementedError("Material.bumpMaps")
+
+        return data
+"""
 
 
 def matrix4x4(translation, rotation, scale=None):
@@ -305,3 +552,8 @@ def matrix4x4(translation, rotation, scale=None):
     for y in range(4):
         for x in range(4):
             yield m[x][y]
+
+
+def rgba_to_hex(color):
+    # MAYBE: https://github.com/mrdoob/three.js/blob/master/src/math/Color.js#L273 ?????
+    return color.b + (color.g << 8) + (color.r << 16)
